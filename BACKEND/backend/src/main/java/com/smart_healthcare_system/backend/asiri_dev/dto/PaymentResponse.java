bcfc32efca856979a7b64707da9dda2b5208532e
asirialwis <asirichathurika900@gmail.com>
2024-10-16 09:02:55 +0530

    payment response

---
+++ BACKEND/backend/src/main/java/com/smart_healthcare_system/backend/asiri_dev/dto/PaymentResponse.java
@@ -0,0 +1,22 @@
+package com.smart_healthcare_system.backend.asiri_dev.dto;
+
+import com.smart_healthcare_system.backend.asiri_dev.model.Payment;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PaymentResponse {
+    private String paymentId;
+    private Payment.PaymentType paymentType;
+    private String cardholderName;
+    private String cardNumber;
+    private String expiryDate;
+    private String cvv;
+    private String remarks;
+    private String status;
+}
