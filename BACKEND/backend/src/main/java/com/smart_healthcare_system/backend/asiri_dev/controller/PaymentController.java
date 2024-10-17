9970c9103353d4f35c8a5ddc4f7f23c0111e141e
asirialwis <asirichathurika900@gmail.com>
2024-10-17 08:52:50 +0530

    enable cross origin for controller

--- BACKEND/backend/src/main/java/com/smart_healthcare_system/backend/asiri_dev/controller/PaymentController.java
+++ BACKEND/backend/src/main/java/com/smart_healthcare_system/backend/asiri_dev/controller/PaymentController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/payments")
+@CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
 public class PaymentController {
 
