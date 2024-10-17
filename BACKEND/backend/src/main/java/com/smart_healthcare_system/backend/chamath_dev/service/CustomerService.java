ca0ed491ca5efb349a1f032c4484e452c5ec5d75
IT22338266 <it22338266@my.sliit.lk>
2024-10-17 22:57:54 +0530

    customer service created (unwanted import removed)

--- BACKEND/backend/src/main/java/com/smart_healthcare_system/backend/chamath_dev/service/CustomerService.java
+++ BACKEND/backend/src/main/java/com/smart_healthcare_system/backend/chamath_dev/service/CustomerService.java
@@ -9,7 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
+
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
