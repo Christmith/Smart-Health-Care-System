b49ea0507a35d78e4f1d2be5ae72eeee585813c1
Kalindu Rashmina <krashmina612@gmail.com>
2024-10-19 00:13:00 +0530

    Appointment model change

--- BACKEND/backend/src/main/java/com/smart_healthcare_system/backend/kalindu_dev/model/Appointment.java
+++ BACKEND/backend/src/main/java/com/smart_healthcare_system/backend/kalindu_dev/model/Appointment.java
@@ -1,10 +1,17 @@
 package com.smart_healthcare_system.backend.kalindu_dev.model;
 
+import com.smart_healthcare_system.backend.chamath_dev.model.Customer;
+import com.smart_healthcare_system.backend.yohan_dev.model.Doctor;
+import com.smart_healthcare_system.backend.yohan_dev.model.HospitalService;
 import lombok.*;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.Date;
 
@@ -17,18 +24,31 @@ public class Appointment {
 
     @Id
     private String appointmentId;
+
     @NonNull
-    private String patientId;
+//    @DBRef
+    @DocumentReference
+    private Customer patientId;
+
     @NonNull
-    private String serviceId;
+//    @DBRef
+    @DocumentReference
+    private HospitalService serviceId;
+
     @NonNull
-    private String doctorId;
+//    @DBRef
+    @DocumentReference
+    private Doctor doctorId;
+
     @NonNull
     private Date appointmentDateTime;
+
     private String appointmentDesc;
     private status appointmentStatus;
+
     @CreatedDate
     private Date createdAt;
+
     @LastModifiedDate
     private Date updatedAt;
 
