397c8ff21c36fcc7618f83a111baaf76e801daea
Kalindu Rashmina <krashmina612@gmail.com>
2024-10-19 00:53:07 +0530

    asiri appointment dto change

--- BACKEND/backend/src/main/java/com/smart_healthcare_system/backend/asiri_dev/dto/AppointmentResponse.java
+++ BACKEND/backend/src/main/java/com/smart_healthcare_system/backend/asiri_dev/dto/AppointmentResponse.java
@@ -1,7 +1,9 @@
 package com.smart_healthcare_system.backend.asiri_dev.dto;
 
 import com.smart_healthcare_system.backend.chamath_dev.dto.CustomerResponse;
+import com.smart_healthcare_system.backend.chamath_dev.model.Customer;
 import com.smart_healthcare_system.backend.yohan_dev.dto.DoctorRes;
+import com.smart_healthcare_system.backend.yohan_dev.model.Doctor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +15,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AppointmentResponse {
     private String appointmentId;
+    private Customer patient;  // Corrected to use Customer
+    private Doctor doctor;    // Corrected to use Doctor
     private String appointmentDesc;
     private String appointmentStatus;
     private String appointmentDateTime;
-    private PatientResponse patient;  // Corrected to use PatientResponse
-    private DoctorResponse doctor;    // Corrected to use DoctorResponse
+
 }
