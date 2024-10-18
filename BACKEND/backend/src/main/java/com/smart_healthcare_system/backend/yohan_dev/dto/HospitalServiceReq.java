7fc7fcc9eba784273a7bac4653b3eed34dfff709
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-10-18 16:06:17 +0530

    create hospitalService req dto

---
+++ BACKEND/backend/src/main/java/com/smart_healthcare_system/backend/yohan_dev/dto/HospitalServiceReq.java
@@ -0,0 +1,25 @@
+package com.smart_healthcare_system.backend.yohan_dev.dto;
+
+import com.smart_healthcare_system.backend.yohan_dev.enums.PaymentOption;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class HospitalServiceReq {
+    private String serviceName;
+    private String serviceDescription;
+    private List<PaymentOption> paymentOption;
+    private String department;
+    private String location;
+    private List<String> selectedDays;
+    private List<String> selectedDoctors;
+    private Map<String, List<Map<String, String>>> timeSlots;
+}
