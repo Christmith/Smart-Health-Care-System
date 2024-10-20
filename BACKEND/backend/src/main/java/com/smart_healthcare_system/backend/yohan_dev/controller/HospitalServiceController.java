4b25abd544db17afcd7f0f85cd5e27b8153850d5
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-10-20 13:45:02 +0530

    Add CrossOrigin annotation to HospitalServiceController

--- BACKEND/backend/src/main/java/com/smart_healthcare_system/backend/yohan_dev/controller/HospitalServiceController.java
+++ BACKEND/backend/src/main/java/com/smart_healthcare_system/backend/yohan_dev/controller/HospitalServiceController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/services")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class HospitalServiceController {
     private final HospitalServiceService hospitalServiceService;
 
