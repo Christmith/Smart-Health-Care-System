a67147f877b60f14cf349dc79c72230a2ade2602
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-10-20 13:45:48 +0530

    Refactor PaymentOption enum values to use camel case

--- BACKEND/backend/src/test/java/com/smart_healthcare_system/backend/yohan_dev/dto/HospitalServiceResTest.java
+++ BACKEND/backend/src/test/java/com/smart_healthcare_system/backend/yohan_dev/dto/HospitalServiceResTest.java
@@ -18,7 +18,7 @@ public class HospitalServiceResTest {
                 .serviceId("1")
                 .serviceName("General Checkup")
                 .serviceDescription("Routine health checkup")
-                .paymentOption(Arrays.asList(PaymentOption.CASH_PAYMENT, PaymentOption.CARD_PAYMENT))
+                .paymentOption(Arrays.asList(PaymentOption.Cash, PaymentOption.Card))
                 .department("General Medicine")
                 .location("Building A")
                 .status(true)
@@ -79,13 +79,13 @@ public class HospitalServiceResTest {
     // Positive and Negative Test for paymentOption
     @Test
     public void test_P_GetPaymentOption() {
-        List<PaymentOption> expectedOptions = Arrays.asList(PaymentOption.CASH_PAYMENT, PaymentOption.CARD_PAYMENT);
+        List<PaymentOption> expectedOptions = Arrays.asList(PaymentOption.Cash, PaymentOption.Card);
         assertThat(hospitalServiceRes.getPaymentOption()).isEqualTo(expectedOptions);
     }
 
     @Test
     public void test_N_GetPaymentOption() {
-        List<PaymentOption> incorrectOptions = Arrays.asList(PaymentOption.CASH_PAYMENT, PaymentOption.INSURANCE_PAYMENT);
+        List<PaymentOption> incorrectOptions = Arrays.asList(PaymentOption.Cash, PaymentOption.Insurance);
         assertThat(hospitalServiceRes.getPaymentOption()).isNotEqualTo(incorrectOptions);
     }
 
