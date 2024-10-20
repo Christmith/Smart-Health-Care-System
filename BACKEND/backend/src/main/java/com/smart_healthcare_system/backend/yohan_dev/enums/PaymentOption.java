33546974e912342dc0891588c3a46b8c41ce3f65
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-10-20 13:45:35 +0530

    Refactor PaymentOption enum values

--- BACKEND/backend/src/main/java/com/smart_healthcare_system/backend/yohan_dev/enums/PaymentOption.java
+++ BACKEND/backend/src/main/java/com/smart_healthcare_system/backend/yohan_dev/enums/PaymentOption.java
@@ -1,8 +1,8 @@
 package com.smart_healthcare_system.backend.yohan_dev.enums;
 
 public enum PaymentOption {
-    CASH_PAYMENT,
-    CARD_PAYMENT,
-    INSURANCE_PAYMENT,
-    FREE_OF_CHARGE
+    Cash,
+    Card,
+    Insurance,
+    Free
 }
